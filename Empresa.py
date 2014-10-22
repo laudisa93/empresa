ac3a842cbcf875eb0f7500da96f2fb478bef6207
laudisa93 <laudisa93@gmail.com>
2014-10-22 20:05:48 +0200

    new class

---
+++ Empresa.py
@@ -0,0 +1,10 @@
+__author__ = 'Laura'
+
+class Empresa:
+
+    def __init__(self, nombre_empresa, cif, razon_social):
+        self.nombre_empresa = nombre_empresa
+        self.cif = cif
+        self.razon_social = razon_social
+        self.departamentos = []
+
